e0f495300e5acd292ea329ab8e44b9333359f364
Santiago Rayn <santiagorayn24@gmail.com>
2025-08-05 21:55:47 -0300

    termino ejercicio 06, 2 soluciones distintas, comentarios añadidos

---
+++ int_python/ejercicios/06_n_invertido.py
@@ -0,0 +1,33 @@
+"""
+Número invertido (3 cifras)
+Pedí un número de 3 cifras y mostrálo al revés.
+---
+Para cumplir con la consigna opte por hacer 2 soluciones completamente distintas:
+    Opción[1]: Utilizar la estructura de control de flujo for en conjunto con la
+    función built-in range(). (Queria complicarme y acordarme de como se usaba range())
+    Opción[2]: Utilizar una operación particular sobre Strings -> String Slicing
+    manera más "pythonica"
+"""
+#Opción1
+def int_inverter_range(n: int)->int: 
+    n_str = str(n)
+    inv_str = ""
+
+    for value in range(len(n_str) - 1, -1, -1):
+        inv_str += n_str[value]
+    
+    result = int(inv_str)
+    return result
+
+""" number = int(input("\nIngrese un número entero porfavor: "))
+print(f"\nEl número invertido es: {int_inverter_range(number)}") """
+
+#Opción2
+def int_inverter_slice(n: int)->int: 
+    n_str = str(n)
+    inv_str = n_str[::-1]
+    result = int(inv_str)
+    return result
+
+number = int(input("\nIngrese un número entero porfavor: "))
+print(f"\nEl número invertido es: {int_inverter_range(number)}")
