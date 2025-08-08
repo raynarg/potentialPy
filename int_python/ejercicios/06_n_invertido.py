8fa8d80a7abbcb8b0d04beca5605cd5058551a8f
Santiago Rayn <santiagorayn24@gmail.com>
2025-08-07 23:19:17 -0300

    ej 07 resuelto, uso de eval()

--- int_python/ejercicios/06_n_invertido.py
+++ int_python/ejercicios/06_n_invertido.py
@@ -8,7 +8,7 @@ Para cumplir con la consigna opte por hacer 2 soluciones completamente distintas
     Opción[2]: Utilizar una operación particular sobre Strings -> String Slicing
     manera más "pythonica"
 """
-#Opción1
+""" #Opción1
 def int_inverter_range(n: int)->int: 
     n_str = str(n)
     inv_str = ""
@@ -17,7 +17,7 @@ def int_inverter_range(n: int)->int:
         inv_str += n_str[value]
     
     result = int(inv_str)
-    return result
+    return result """
 
 """ number = int(input("\nIngrese un número entero porfavor: "))
 print(f"\nEl número invertido es: {int_inverter_range(number)}") """
@@ -30,4 +30,4 @@ def int_inverter_slice(n: int)->int:
     return result
 
 number = int(input("\nIngrese un número entero porfavor: "))
-print(f"\nEl número invertido es: {int_inverter_range(number)}")
+print(f"\nEl número invertido es: {int_inverter_slice(number)}")
